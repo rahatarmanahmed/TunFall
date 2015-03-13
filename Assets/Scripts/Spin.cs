87cfe2e47b8c9ff417feb7175f520cff6ac0685d
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-03-13 03:46:00 -0500

    Adjusted lighting settings

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	public Vector3 spin = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().angularVelocity = spin;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/TunGen.cs
+++ Assets/Scripts/TunGen.cs
@@ -24,6 +24,7 @@ public class TunGen : MonoBehaviour {
 		TunSegment segment = obj.AddComponent<TunSegment>();
 		segment.CreateMesh (top, bottom, numSides, material);
 		obj.transform.parent = transform;
+		obj.layer = gameObject.layer;
 	}
 
 	void CreateNextSegment()
